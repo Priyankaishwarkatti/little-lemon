99e728cb139aa3dd2ac8704d5d09488f579174ef
Priyanka Ishwarkatti <priyankaishwarkatti22@gmail.com>
2025-09-04 23:21:37 +0530

    creating footer for little lemon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+    <footer className="bg-gray-900 text-white text-center p-4 mt-10">
       <p>© 2025 Little Lemon Restaurant</p>
     </footer>
   );
