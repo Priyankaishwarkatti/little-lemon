71f9bb0c14f1781310780322802b3eb4f8379060
Priyanka Ishwarkatti <priyankaishwarkatti22@gmail.com>
2025-09-18 23:13:58 +0530

    Button text change

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -39,7 +39,7 @@ function BookingForm({ addBooking }) {
         <label>Guests:</label>
         <input type="number" min="1" value={guests} onChange={(e) => setGuests(e.target.value)} required />
 
-        <button type="submit">Book Table</button>
+        <button type="submit">Reserve Table</button>
       </form>
     </div>
     </div>
